197cc9acdbb1de66f35f314ada1930ad380ddec8
Ahmed Cruz <ahmedskynni@gmail.com>
2024-04-10 19:02:25 -0600

    RAMA del pinshi ahmeeeeed

--- Populacion/individuo.cpp
+++ Populacion/individuo.cpp
@@ -1,77 +1,101 @@
 #include "individuo.hpp"
 #include <iostream>
 #include <math.h>
+
 // Implementación del constructor de individuo
-individuo::individuo(int tamCuadrado) {
+individuo::individuo(int tamCuadrado)
+{
     // Aquí iría la lógica para inicializar tu objeto individuo con el tamaño N
-    this->tamCuadrado=tamCuadrado;
+    this->tamCuadrado = tamCuadrado;
     cromosomas = *new cromosoma(tamCuadrado);
-    std::cout<<cromosomas;
+    std::cout << cromosomas;
 }
 
 // Implementación del destructor de individuo
-individuo::~individuo() {
+individuo::~individuo()
+{
     // Aquí iría la lógica para liberar los recursos utilizados por tu objeto individuo
 }
 
-
-//Implemntacion cruza
-void individuo::cruza(individuo uno, individuo dos){
-
+// Implemntacion cruza
+void individuo::cruza(individuo uno, individuo dos)
+{
 }
 
-
-
-//Metodo maximizacion
-int individuo::func1(std::vector<int> fil,std::vector<int> col,int diag){
+// Metodo maximizacion
+int individuo::func1(std::vector<int> fil, std::vector<int> col, int diag)
+{
 
     int numExitos;
-    for (int i = 0; i < tamCuadrado; i++){
-        if ( fil.at(i) == this->cromosomas.getNumMagico() ) numExitos++;
-        if ( col.at(i) == this->cromosomas.getNumMagico() ) numExitos++;
+    for (int i = 0; i < tamCuadrado; i++)
+    {
+        if (fil.at(i) == this->cromosomas.getNumMagico())
+            numExitos++;
+        if (col.at(i) == this->cromosomas.getNumMagico())
+            numExitos++;
     }
-    
-    if (diag==this->cromosomas.getNumMagico()) numExitos++;
-    
+
+    if (diag == this->cromosomas.getNumMagico())
+        numExitos++;
+
     return numExitos;
 }
-//Metodo de minimizacion
-bool individuo::func2(std::vector<int> fil,std::vector<int> col,int diag){
-    int exitoGeneral=2*tamCuadrado+1;
+
+// Metodo de minimizacion
+int individuo::func2(std::vector<int> fil, std::vector<int> col, int diag)
+{
+
     int numExitos;
-    int sumafil=0;
-    int sumcol=0;
-    for (int i : fil){
-        sumafil += abs(this->cromosomas.getNumMagico() -i );
+    int sumafil = 0;
+    int sumcol = 0;
+    for (int i : fil)
+    {
+        sumafil += abs(this->cromosomas.getNumMagico() - i);
     }
-    
-    for (int i : col){
-        sumcol += abs(this->cromosomas.getNumMagico() -i );
+
+    for (int i : col)
+    {
+        sumcol += abs(this->cromosomas.getNumMagico() - i);
     }
-    
-    numExitos = sumafil + sumcol + abs(this->cromosomas.getNumMagico() -diag);
-    if (numExitos==0) return true;
-    else return false;
+
+    numExitos = sumafil + sumcol + abs(this->cromosomas.getNumMagico() - diag);
+
+    return numExitos;
 }
 
-
-
-
-/* 
+/*
 std::ostream& operator<<(std::ostream& os, individuo indi){
 
 } */
 
 int main()
 {
-    
-    individuo miPoblacion(5); 
+    int numPoblaciones, tamCuadrado;
 
-    if(miPoblacion.func1(miPoblacion.cromosomas.sumaFilas(),miPoblacion.cromosomas.sumaColumnas(),miPoblacion.cromosomas.sumaDiagonales()))
-        std::cout<<"COOOL";
+    std::cout << "Ingrese el numero de poblaciones que desea generar: ";
+    std::cin >> numPoblaciones;
+    std::cout << "Ingrese el tamano del cuadrado para la poblacion: ";
+    std::cin >> tamCuadrado;
+
+    std::vector<individuo> poblacion;
+
+    for (int i = 1; i <= numPoblaciones; i++)
+    {
+        individuo miPoblacion(tamCuadrado);
+        poblacion.push_back(miPoblacion);
+
+        if (miPoblacion.func1(miPoblacion.cromosomas.sumaFilas(), miPoblacion.cromosomas.sumaColumnas(), miPoblacion.cromosomas.sumaDiagonales()))
+            std::cout << "COOOL";
         else
-        std::cout<<"NOCOOOL";
-    
-    
+            std::cout << "NOCOOOL";
+    }
+
     return 0;
 }
+
+// while (exitosGeneral!=numexitos)
+//     {
+
+//         torneobinario(miPoblacion.func1(miPoblacion.cromosomas.sumaFilas(),miPoblacion.cromosomas.sumaColumnas(),miPoblacion.cromosomas.sumaDiagonales()))
+
+//     }
